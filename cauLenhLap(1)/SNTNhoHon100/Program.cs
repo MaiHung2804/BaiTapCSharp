5f675a0e269facfa3e1db92e31604d4cab9d1543
MaiHung2804 <maihung2804jr@gmail.com>
2025-03-17 18:55:40 +0700

    BTVN

---
+++ cauLenhLap(1)/SNTNhoHon100/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNTNhoHon100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Danh sach STN < 100: ");
+            int dem = 0;
+            for( int i=2; i<100; i++)
+            {
+                for(int j=0; j<=i; j++)
+                {
+                    if (i%j == 0)
+                    {
+                        dem++;
+                    }
+                }
+                if (dem == 2)
+                {
+                    Console.WriteLine(i);
+                }
+                dem = 0;
+            }
+        }
+    }
+}
---
+++ cauLenhLap(1)/vayLai/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vayLai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = 1.0;
+            int month = 1;
+            double intersetRate = 1.0;
+            Console.WriteLine("Nhap so tien: ");
+            money = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so thang gui: ");
+            month = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so % lai suat: ");
+            intersetRate = double.Parse(Console.ReadLine());
+
+            double totalInterset = 0.0;
+            for (int i=0; i<month; i++)
+            {
+                totalInterset += money * (intersetRate/100) / 12 ;
+            }
+            Console.WriteLine($"Tong so tien nhan:{totalInterset:F2} ");
+        }
+    }
+}
