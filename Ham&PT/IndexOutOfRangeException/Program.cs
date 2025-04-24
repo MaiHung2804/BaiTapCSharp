b714c616ca6130d2b4f568356a90042f7db8b810
maihung2804 <maihung2804jr@gmail.com>
2025-04-24 22:35:22 +0700

    no message

---
+++ Ham&PT/IndexOutOfRangeException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexOutOfRangeException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> characters = new List<char>();
+            characters.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+
+            if (characters.Count > 6)
+            {
+                char value = characters[6];
+                Console.WriteLine("Gia tri tai vi tri 6: " + value);
+            }
+            else
+            {
+                Console.WriteLine("Danh sach khong du phan tu de truy cap vi tri 6.");
+            }
+        }
+    }
+}
